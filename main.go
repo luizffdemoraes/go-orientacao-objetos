450607be62b17299f5c7bc99b86fd95ed12b5596
Luiz Felipe <lffm1994@gmail.com>
2024-01-20 19:22:13 -0300

    Criando o método sacar

--- main.go
+++ main.go
@@ -9,49 +9,23 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+    if podeSacar {
+        c.saldo -= valorDoSaque
+        return "Saque realizado com sucesso"
+    } else {
+        return "Saldo insuficiente"
+    }
+}
+
 func main() {
-	// conta := ContaCorrente{
-	// 	titular:       "Luiz",
-	// 	numeroAgencia: 589,
-	// 	numeroConta:   123456,
-	// 	saldo:         125.50,
-	// }
+	var contaDaSilvia = ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	// conta2 := ContaCorrente{
-	// 	titular:       "Luiz",
-	// 	numeroAgencia: 589,
-	// 	numeroConta:   123456,
-	// 	saldo:         125.50,
-	// }
+	fmt.Println(contaDaSilvia.saldo)
 
-	// Validar igualdade
-	// fmt.Println(conta == conta2)
-
-	// contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
-
-	// fmt.Println(conta)
-	// fmt.Println(contaDaBruna)
-
-	// Ponteiro *
-	var contaDaCris *ContaCorrente
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
-	contaDaCris.numeroConta = 111333
-	contaDaCris.numeroAgencia = 300
-
-	var contaDaCris2 *ContaCorrente
-	contaDaCris2 = new(ContaCorrente)
-	contaDaCris2.titular = "Cris"
-	contaDaCris2.saldo = 500
-	contaDaCris2.numeroConta = 111333
-	contaDaCris2.numeroAgencia = 300
-
-	// Visualizar o endereço
-	fmt.Println(&contaDaCris)
-
-	// Valída o endereço
-	fmt.Println(contaDaCris == contaDaCris2)
-	// Valída o conteúdo
-	fmt.Println(*contaDaCris == *contaDaCris2)
+    fmt.Println(contaDaSilvia.Sacar(400))
+    fmt.Println(contaDaSilvia.saldo)
 }
