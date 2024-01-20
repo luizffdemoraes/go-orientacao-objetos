e87396768c3afd4659b9a893331a707c58848586
Luiz Felipe <lffm1994@gmail.com>
2024-01-20 17:53:17 -0300

    New e ponteiros

--- main.go
+++ main.go
@@ -17,8 +17,18 @@ func main() {
 		saldo:         125.50,
 	}
 
-    contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
 
 	fmt.Println(conta)
-    fmt.Println(contaDaBruna)
+	fmt.Println(contaDaBruna)
+
+	// Ponteiro *
+	var contaDaCris *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
+	contaDaCris.numeroConta = 111333
+	contaDaCris.numeroAgencia = 300
+
+	fmt.Println(*contaDaCris)
 }
