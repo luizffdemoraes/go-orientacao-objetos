79e30ffa1e7419e0c1c20e89c0c51bbbe181aba0
Luiz Felipe <lffm1994@gmail.com>
2024-01-21 01:10:37 -0300

    Alterando a visibilidade

--- banco/main.go
+++ banco/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"banco/clientes"
 	"banco/contas"
 	"fmt"
 )
 
 func main() {
-	contaDaSilvia := contas.ContaCorrente{Titular: clientes.Titular{Nome: "Silvia", CPF: "123.111.123.12", Profissao: "Desenvolvedor"}, NumeroConta: 123, NumeroAgencia: 123456, Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: clientes.Titular{Nome: "Gustavo", CPF: "123.111.123.13", Profissao: "Tech Leader"}, NumeroConta: 123, NumeroAgencia: 123456, Saldo: 300}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	// status := contaDaSilvia.Transferir(200, &contaDoGustavo)
-	status := contaDoGustavo.Transferir(-200, &contaDaSilvia)
-
-	fmt.Println(status)
-	fmt.Println(contaDaSilvia)
-	fmt.Println(contaDoGustavo)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
