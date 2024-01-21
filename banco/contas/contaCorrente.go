d152def48fe9620ef405c398f2873e53349fc4b0
Luiz Felipe <lffm1994@gmail.com>
2024-01-21 00:29:31 -0300

    Composição de structs

--- banco/contas/contaCorrente.go
+++ banco/contas/contaCorrente.go
@@ -1,7 +1,9 @@
 package contas
 
+import "banco/clientes"
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
 	Saldo         float64
@@ -9,29 +11,29 @@ type ContaCorrente struct {
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
-    if podeSacar {
-        c.Saldo -= valorDoSaque
-        return "Saque realizado com sucesso"
-    } else {
-        return "Saldo insuficiente"
-    }
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-    if valorDoDeposito > 0 {
-        c.Saldo += valorDoDeposito
-        return "Deposito realizado com sucesso", c.Saldo
-    } else { 
-        return "Valor do depósito menor que zero", c.Saldo
-    }
+	if valorDoDeposito > 0 {
+		c.Saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.Saldo
+	} else {
+		return "Valor do depósito menor que zero", c.Saldo
+	}
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-    if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-        c.Saldo -= valorDaTransferencia 
-        contaDestino.Depositar(valorDaTransferencia)
-        return true
-    } else {
-        return false
-    }
+	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
+		c.Saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
 }
